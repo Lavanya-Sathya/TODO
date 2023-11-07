f8a19235b7a615871cf6e832f30a88f78a3862f4
Lavanya-Sathya <sathyalavanya5@gmail.com>
2023-11-08 00:30:38 +0530

    login-signup-update

--- script/login.js
+++ script/login.js
@@ -2,70 +2,78 @@ let emailError = document.getElementById("email-error");
 let passError = document.getElementById("pass-error");
 let messageError = document.getElementById("submit-error");
 let emailVal = document.getElementById("email");
-let passwordVal = document.getElementById('pwd');
+let passwordVal = document.getElementById("pwd");
 
-
+let eyeicon = document.getElementById("eyeicon");
+eyeicon.addEventListener("click", () => {
+  if (passwordVal.type === "password") {
+    eyeicon.innerHTML = `<i class="bi bi-eye-fill"></i>`;
+    passwordVal.type = "text";
+  } else {
+    eyeicon.innerHTML = `<i class="bi bi-eye-slash-fill"></i>`;
+    passwordVal.type = "password";
+  }
+});
 function validateEmail() {
-    let email = emailVal.value;
-    if (email.length == 0) {
-        emailError.innerHTML = "Email is required";
-        return false;
-    }
-    if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
-        emailError.innerHTML = "Email is invalid";
-        return false;
-    }
-    emailError.innerHTML = "";
-    return true;
-
+  let email = emailVal.value;
+  if (email.length == 0) {
+    emailError.innerHTML = "Email is required";
+    return false;
+  }
+  if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+    emailError.innerHTML = "Email is invalid";
+    return false;
+  }
+  emailError.innerHTML = "";
+  return true;
 }
 
 function validatePassword() {
-    let password = passwordVal.value;
+  let password = passwordVal.value;
 
-    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
-    if (password === '') {
-        passError.innerHTML = "password is required"
-        return false;
-    }
-    else if (!password.match(passwordPattern)) {
-        passError.innerHTML = 'Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, and one digit.';
-        return false;
-    } 
-    passError.innerHTML = "";
-    return true;
-
+  if (password === "") {
+    passError.innerHTML = "password is required";
+    return false;
+  } else if (!password.match(passwordPattern)) {
+    passError.innerHTML =
+      "Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, and one digit.";
+    return false;
+  }
+  passError.innerHTML = "";
+  return true;
 }
 
-function validateForm(){
-    if( !validateEmail() || !validatePassword()){
-        messageError.style.display="block";
-        messageError.innerHTML="Please fix the error";
-        
-        setTimeout(()=>{messageError.style.display="none";},3000);
-        return false;
-    }
-    else{
-        const data = JSON.parse(localStorage.getItem("data")) || [];
-        if(data.length === 0){
-            emailVal.value = "";
-            passwordVal.value = "";
-            alert("Please Sign Up")
-            return false;
-        }
-        else if(emailVal.value === data[0].username && passwordVal.value=== data[0].password){
-            alert("LoggedIn Successfully");
+function validateForm() {
+  if (!validateEmail() || !validatePassword()) {
+    messageError.style.display = "block";
+    messageError.innerHTML = "Please fix the error";
 
-        }
-        else{
-            messageError.style.display="block";
-            messageError.innerHTML="Password and Email doesn't match. Please Check";
-            messageError.style.color = "red";
-            setTimeout(()=>{messageError.style.display="none";},3000);
-            return false;
-        }
-
+    setTimeout(() => {
+      messageError.style.display = "none";
+    }, 3000);
+    return false;
+  } else {
+    const data = JSON.parse(localStorage.getItem("data")) || [];
+    if (data.length === 0) {
+      emailVal.value = "";
+      passwordVal.value = "";
+      alert("Please Sign Up");
+      return false;
+    } else if (
+      emailVal.value === data[0].username &&
+      passwordVal.value === data[0].password
+    ) {
+      alert("LoggedIn Successfully");
+    } else {
+      messageError.style.display = "block";
+      messageError.innerHTML = "Password and Email doesn't match. Please Check";
+      messageError.style.color = "red";
+      setTimeout(() => {
+        messageError.style.display = "none";
+      }, 3000);
+      return false;
     }
-   
+  }
 }
--- script/sign.js
+++ script/sign.js
@@ -9,102 +9,108 @@ let emailIcon = document.getElementById("email-error-icon");
 let passIcon = document.getElementById("pass-error-icon");
 let repassIcon = document.getElementById("repass-error-icon");
 var emailVal = document.getElementById("email");
-let passwordVal = document.getElementById('pwd');
+let passwordVal = document.getElementById("pwd");
 
 function validateName() {
-    var name = document.getElementById("name").value;
-    nameIcon.style.display="block";
-    if (name.length == 0) {
-        nameIcon.innerHTML = `<i class="fa-solid fa-circle-xmark " style="color: #b80a0a;"></i>`;
-        return false;
-    }
-    if (!name.match(/^[A-za-z]*\s{1}[A-Za-z]*$/)) {
-        nameIcon.innerHTML = `<i class="fa-solid fa-circle-xmark " id="icon" style="color: #b80a0a;"></i>`;
-        return false;
-    }
-    nameIcon.innerHTML = `<i class="fa-solid fa-check" style="color: #16ca55;"></i>`;
-    return true;
+  var name = document.getElementById("name").value;
+  nameIcon.style.display = "block";
+  if (name.length == 0) {
+    nameIcon.innerHTML = `<i class="fa-solid fa-circle-xmark " style="color: #b80a0a;"></i>`;
+    nameError.innerHTML = "Name is required";
+    return false;
+  }
+  if (name.length < 4) {
+    nameIcon.innerHTML = `<i class="fa-solid fa-circle-xmark " id="icon" style="color: #b80a0a;"></i>`;
+    nameError.innerHTML = "Name must be atleast 4 characters";
+    return false;
+  }
+  nameIcon.innerHTML = `<i class="fa-solid fa-check" style="color: #16ca55;"></i>`;
+  nameError.innerHTML = "";
+  return true;
 }
 
 function validateEmail() {
-    var email = emailVal.value;
-    emailIcon.style.display="block";
-    if (email.length == 0) {
-        // emailError.innerHTML = "Email is required";
-        emailIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
-        return false;
-    }
-    if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
-        // emailError.innerHTML = "Email is invalid";
-        emailIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
-        return false;
-    }
-    emailIcon.innerHTML = `<i class="fa-solid fa-check" style="color: #16ca55;"></i>`;
-    return true;
-
+  var email = emailVal.value;
+  emailIcon.style.display = "block";
+  if (email.length == 0) {
+    emailError.innerHTML = "Email is required";
+    emailIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
+    return false;
+  }
+  if (!email.match(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/)) {
+    emailError.innerHTML = "Email is invalid";
+    emailIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
+    return false;
+  }
+  emailIcon.innerHTML = `<i class="fa-solid fa-check" style="color: #16ca55;"></i>`;
+  emailError.innerHTML = "";
+  return true;
 }
 
 function validatePassword() {
-    const password = passwordVal.value;
-    passIcon.style.display="block";
-    const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+  const password = passwordVal.value;
+  passIcon.style.display = "block";
+  const passwordPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 
-    if (password === '') {
-        // passError.innerHTML = "password is required"
-        passIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
+  if (password === "") {
+    passError.innerHTML = "password is required";
+    passIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
 
-        return false;
-    }
-    else if (!password.match(passwordPattern)) {
-        // passError.innerHTML = 'Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, and one digit.';
-        passIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
-        return false;
-    } 
-    // passError.innerHTML = "Valid";
-    passIcon.innerHTML = `<i class="fa-solid fa-check" style="color: #16ca55;"></i>`;
-    return true;
-
+    return false;
+  } else if (!password.match(passwordPattern)) {
+    passError.innerHTML =
+      "Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, and one digit.";
+    passIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
+    return false;
+  }
+  passError.innerHTML = "";
+  passIcon.innerHTML = `<i class="fa-solid fa-check" style="color: #16ca55;"></i>`;
+  return true;
 }
 
-function validateCon(){
-    const password = document.getElementById('pwd').value;
-    const confirmPassword = document.getElementById('repwd').value;
-    repassIcon.style.display="block";
+function validateCon() {
+  const password = document.getElementById("pwd").value;
+  const confirmPassword = document.getElementById("repwd").value;
+  repassIcon.style.display = "block";
 
-    if (password === '') {
-        // repassError.innerHTML = "password is required"
-        repassIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
-        return false;
-    }
-    else if (password !== confirmPassword) {
-        // repassError.innerHTML = "Passwords do not match"
-        repassIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
-        return false;
-    } 
-    // repassError.innerHTML = "Valid";
-    repassIcon.innerHTML = `<i class="fa-solid fa-check" style="color: #16ca55;"></i>`;
-    return true;
+  if (password === "") {
+    repassError.innerHTML = "password is required";
+    repassIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
+    return false;
+  } else if (password !== confirmPassword) {
+    repassError.innerHTML = "Passwords do not match";
+    repassIcon.innerHTML = `<i class="fa-solid fa-circle-xmark" style="color: #b80a0a;"></i>`;
+    return false;
+  }
+  repassError.innerHTML = "";
+  repassIcon.innerHTML = `<i class="fa-solid fa-check" style="color: #16ca55;"></i>`;
+  return true;
 }
 
-function validateForm(){
-    if(!validateName() || !validateEmail() || !validatePassword() || !validateCon()){
-        messageError.style.display="block";
-        messageError.innerHTML="Please fix the error";
-        
-        setTimeout(()=>{messageError.style.display="none";},3000);
-        return false;
-    }
-    else{
-        const userName = emailVal.value;
-        const passworsName = passwordVal.value;
-        let data = [
-          {
-            "username": userName,
-            "password": passworsName
-          }
-        ];
-        localStorage.setItem("data", JSON.stringify(data));
-        return true;
-    }
-    
+function validateForm() {
+  if (
+    !validateName() ||
+    !validateEmail() ||
+    !validatePassword() ||
+    !validateCon()
+  ) {
+    messageError.style.display = "block";
+    messageError.innerHTML = "Please fix the error";
+
+    setTimeout(() => {
+      messageError.style.display = "none";
+    }, 3000);
+    return false;
+  } else {
+    const userName = emailVal.value;
+    const passworsName = passwordVal.value;
+    let data = [
+      {
+        username: userName,
+        password: passworsName,
+      },
+    ];
+    localStorage.setItem("data", JSON.stringify(data));
+    return true;
+  }
 }
